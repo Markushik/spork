3a45f49af060bf405ae0cff6a681a2ada67d597b
Mark <zemly@duck.com>
2025-01-30 03:41:36 -0800

    Add Elixir to devShell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "my epic vims collection";
+
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+  in
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.neovim
+          pkgs.elixir
+        ];
+
+        shellHook = ''
+          echo "h Mark!"
+        '';
+      };
+    });
+}
+
